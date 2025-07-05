1e270290d1885a4d2230fb2ef4642ba35f04ffec
owocc <owocc@qq.com>
2025-07-06 06:16:09 +0800

    ✨feat: Implement Image Component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightThemeRapide from "starlight-theme-rapide";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from 'astro-auto-import';
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
     integrations: [
+        AutoImport({
+            imports: [
+                {
+                    "./src/components/image.astro": [['default', 'Image']],
+                }
+            ]
+        }),
         starlight({
             plugins: [starlightThemeRapide()],
             title: "Next Docs",
@@ -16,6 +28,9 @@ export default defineConfig({
                     href: "https://github.com/withastro/starlight",
                 },
             ],
+            customCss: [
+                './src/styles/global.css'
+            ]
         }),
         mdx(),
     ],
